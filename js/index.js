87ec8d4c5c7e7a824500b1246844abd86405d61f
Ícaro Almeida <62527468+icaroregis@users.noreply.github.com>
2022-03-15 21:07:12 -0300

    menu mobile finalizado

--- js/index.js
+++ js/index.js
@@ -1,1 +1,9 @@
-const menuMobile = document.querySelector(''); 
+let show = true; 
+const showMenu = document.querySelector('.showMenu');   
+const buttonMenuHeader = document.querySelector('.buttonMenuHeader');   
+
+buttonMenuHeader.addEventListener('click', () => {
+  document.body.style.overflow = show ? "hidden" : "initial";
+  showMenu.classList.toggle("on", show); 
+  show = !show;  
+})  
